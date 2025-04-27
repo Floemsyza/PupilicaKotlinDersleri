f80c57e9543240fafde6c2976d12e298fb10554f
Loe <feyzairemkart192@gmail.com>
2025-04-27 23:52:43 +0300

    Variables

---
+++ app/src/main/java/com/loem/kotlindersleri/degiskenler/Odev1.kt
@@ -0,0 +1,154 @@
+package com.loem.kotlindersleri.degiskenler
+
+fun main(){
+    var sehir = "İstanbul"
+    println("Şehir: $sehir")
+
+    var ulke = "Türkiye"
+    println("Ülke: $ulke")
+
+    var telefon = 0123456789
+    val telefonString = telefon.toString()
+    println("Telefon: $telefonString")
+
+    var postaKodu = 34000
+    val PostaKoduString = postaKodu.toString()
+    println("Posta Kodu: $PostaKoduString")
+
+    var email = "example@example.com"
+    println(email)
+
+    var meslek = "Bilgisayar Mühendisliği Öğrencisi"
+    println("Meslek: $meslek")
+
+    val stokMiktari = 90
+    println("Stok Miktarı: $stokMiktari")
+
+    var musteriAdi = "Loem"
+    println(musteriAdi)
+
+    var bakiye = 1.000
+    println("Bakiye: $bakiye")
+
+    var dogumGunu = "30.10.2005"
+    println("Doğum Günü: $dogumGunu")
+
+    var maas = 50.000
+    println("Maaş: $maas")
+
+    var medeniDurum = false
+    if(medeniDurum){
+        println("Evli")
+    }else{
+        println("Bekar")
+    }
+
+    var urunYorum = "Harika bir ürün, bayıldım"
+    println(urunYorum)
+
+    val odemeTarihi = "25.12.2025"
+    println("Ödeme Tarihi: $odemeTarihi")
+
+    var odeme = true
+    if(odeme){
+        println("Ödendi")
+    }else{
+        println("Ödenmedi")
+    }
+
+    val siparisAdedi = 30000
+    println("Sipariş Adedi: $siparisAdedi")
+
+    val arabaModeli = "Megane"
+    println("Araba Modeli: $arabaModeli")
+
+    var kitapAdi = "Suç ve Ceza"
+    println("Kitap Adı: $kitapAdi")
+
+    val yayinlanmaTarihi = "27.04.2025"
+    println(yayinlanmaTarihi)
+
+    var indirimMiktari = "%10"
+    println("İndirim Miktarı: $indirimMiktari")
+
+    val odaSayisi = 3
+    println("Oda Sayısı: $odaSayisi")
+
+    var enlem = 48.8566
+    println("Enlem: $enlem")
+
+    var boylam = 2.3524
+    println("Boylam: $boylam")
+
+    var urunAdi = "Televizyon"
+    println("Ürün Adı: $urunAdi")
+
+    val yemekFiyati = 1000.00
+    println("Yemek Fiyatı: $yemekFiyati")
+
+    var marka = "Bosch"
+    println("Marka: $marka")
+
+    var muzikAdı = "Fikrimin İnce Gülü"
+    println("Müzik Adı: $muzikAdı")
+
+    val videoSüresi = "3.10"
+    println("Video Süresi: $videoSüresi")
+
+    var urunPuani = 4.7
+    println("Ürün Puanı: $urunPuani")
+
+    val resimAdi = "Yıldızlı Gece"
+    println("Resim Adı: $resimAdi")
+
+    var dosyaFormati = "pdf"
+    println("Dosya Formati: $dosyaFormati")
+
+    var renk = "mavi"
+    println("Renk: $renk")
+
+    var renkKodu = "#0000FF"
+    println("Renk Kodu: $renkKodu")
+
+    val telefonModeli = "iPhone 13"
+    println("Telefon Modeli: $telefonModeli")
+
+    var ekranBoyutu = "6.1 inç"
+    println("Ekran Boyutu: $ekranBoyutu")
+
+    var agirlik = "194 gram"
+    println("Ağırlık: $agirlik")
+
+    val ulusalGun = "23 Nisan"
+    println("Ulusal Gün: $ulusalGun")
+
+    var tatilGunu = ulusalGun
+    println("Tatil Günü: $tatilGunu")
+
+    val rezervasyonTarihi = "15.08.2023"
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+
+    var sokakAdi = "Kahraman Sokak"
+    println("Sokak Adı: $sokakAdi")
+
+    val otobusHatti = "13M"
+    println("Otobüs Hatti: $otobusHatti")
+
+    var kalanDakika = 3
+    println("Kalan Dakika: $kalanDakika")
+
+    val takipKodu = "A1B2C3"
+    println("Takip Kodu: $takipKodu")
+
+    var kuponSuresi = "3 gün"
+    println("Kupon Süresi: $kuponSuresi")
+
+    val kuponKodu = "A1B2C3"
+    println("Kupon Kodu: $kuponKodu")
+    
+    val faturaAdresi = "İstanbul/Kadıköy"
+    println("Fatura Adresi: $faturaAdresi")
+
+
+
+}
